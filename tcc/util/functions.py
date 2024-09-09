58b924dbad869ed210acd189366a41b241776ad2
LucasTeixeira793 <lucas.teixeira@bandtec.com.br>
2024-09-08 21:36:03 -0300

    evolução modelos

--- tcc/util/functions.py
+++ tcc/util/functions.py
@@ -1,5 +1,6 @@
 # %%
 import pandas as pd
+import numpy as np
 import yfinance as yf
 from statsmodels.tsa.seasonal import seasonal_decompose
 
@@ -10,9 +11,20 @@ def get_data_ticker(ticker, period='5y', rolling=1):
     data_hist['prev-day-1'] = data_hist['Close'].rolling(rolling).mean().shift(1)
     data_hist['prev-day-2'] = data_hist['Close'].rolling(rolling).mean().shift(2)
     data_hist['prev-day-3'] = data_hist['Close'].rolling(rolling).mean().shift(3)
-    data_hist['mm_5'] = data_hist['Close'].rolling(5).mean()
-    data_hist['mm_21'] = data_hist['Close'].rolling(21).mean()
+    data_hist['ma_5'] = data_hist['Close'].rolling(21).mean()
+    data_hist['ma_21'] = data_hist['Close'].rolling(21).mean()
+    
+    weights = pd.Series(range(1, 22))
+    data_hist['wma_21'] = data_hist['Close'].rolling(21).apply(lambda prices: np.dot(prices, weights) / weights.sum(), raw=True)
+
+
+    delta = data_hist['Close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(14).mean()  # Ganhos médios
+    loss = (-delta.where(delta < 0, 0)).rolling(14).mean()  # Perdas médias
+    rs = gain / loss  # Strength relativa
+    data_hist['rsi'] = 100 - (100 / (1 + rs))
     data_hist = data_hist.dropna()
+
     data_hist['tomorrow'] = data_hist['Close'].rolling(rolling).mean().shift(-1)
 
     return data_hist
